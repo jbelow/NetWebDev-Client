efd6aefff84fec44d3e4d95e7cb0bf3ee1e4a2f0
Joshua Below <33503583+jbelow@users.noreply.github.com>
2021-02-04 11:35:03 -0600

    the animate thing is now random

--- js/balloons.js
+++ js/balloons.js
@@ -1,4 +1,15 @@
 $(function () {
+
+    //pciking a random class for the header
+    window.onload = function ranHeaderClass() {
+        let animateList = ['animate__animated animate__flash', 'animate__animated animate__headShake', 'animate__animated animate__heartBeat', 'animate__animated animate__tada'];
+
+        animateRandom = animateList[Math.floor(Math.random()*animateList.length)];
+
+        $('#happyHeader').addClass(animateRandom);
+
+        console.log(animateRandom);
+    }
     $('#birthday').pickadate({ format: 'mmmm, d' });
 
     // uncheck all checkboxes (FireFox)
@@ -15,4 +26,10 @@ $(function () {
             $('#' + this.id + 'Img').removeClass().addClass('animate__animated animate__bounceInDown') :
             $('#' + this.id + 'Img').addClass('animate__animated animate__bounceOutUp');
     });
+
+    //event listener for the submit button
+    $('#submit').click(function() {
+        alert( "Handler for .click() called." );
+      });
+
 });
